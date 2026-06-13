1d75943b67a736bc35faa48f8f7c58fd75d18c6c
GreezeTheGoat <allankennedy314@gmail.com>
2026-06-14 00:24:00 +0100

    Added 3 more models: online_security, online_backup, tech_support

--- src/predicting.py
+++ src/predicting.py
@@ -1,7 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
 import pandas as pd
 import yaml
 from pathlib import Path
 import joblib
+import useful_funcs
+
 
 # ========== Opening config file ==========
 
@@ -23,6 +28,9 @@ path_processed = Path(config["paths"]["processed"])
 path_model_fiber = Path(config["paths"]["models"]).joinpath("fiber.pkl")
 path_model_str_tv = Path(config["paths"]["models"]).joinpath("str_tv.pkl")
 path_model_str_movies = Path(config["paths"]["models"]).joinpath("str_movies.pkl")
+path_model_online_backup = Path(config["paths"]["models"]).joinpath("online_backup.pkl")
+path_model_online_security = Path(config["paths"]["models"]).joinpath("online_security.pkl")
+path_model_tech_support = Path(config["paths"]["models"]).joinpath("tech_support.pkl")
 
 path_predictions = Path(config["paths"]["predictions"])
 
@@ -73,6 +81,7 @@ def predict_str_tv(df, model_path, save_csv=False):
     
     return df_prediction_str_tv
 
+
 def predict_str_movies(df, model_path, save_csv=False):
     df_no_str_movies = df[df["streaming_movies"] == 0].drop(columns="streaming_movies")
 
@@ -94,9 +103,81 @@ def predict_str_movies(df, model_path, save_csv=False):
     return df_prediction_str_movies
 
 
+def predict_online_security(df, model_path, save_csv=False):
+    df_no_online_security = df[df["online_security"] == 0].drop(columns="online_security")
+
+    # ========== loading model and creating predictions ==========
+    
+    model_online_security = joblib.load(model_path)
+    
+    pred_prob = model_online_security.predict_proba(df_no_online_security)[:, 1]
+
+    # ========== creating prediction dataframe and exporting as a csv ==========
+    
+    df_prediction_online_security = pd.DataFrame({
+        "Online Security": pred_prob
+    }, index=df_no_online_security.index).sort_values(by="Online Security", ascending=False)
+    
+    if save_csv:
+        df_prediction_online_security.to_csv(path_predictions.joinpath("online_security.csv"), index="customer_id")
+    
+    return df_prediction_online_security
+
+
+def predict_online_backup(df, model_path, save_csv=False):
+    df_no_online_backup = df[df["online_backup"] == 0].drop(columns="online_backup")
+
+    # ========== loading model and creating predictions ==========
+    
+    model_online_backup = joblib.load(model_path)
+    
+    pred_prob = model_online_backup.predict_proba(df_no_online_backup)[:, 1]
+
+    # ========== creating prediction dataframe and exporting as a csv ==========
+    
+    df_prediction_online_backup = pd.DataFrame({
+        "Online Backup": pred_prob
+    }, index=df_no_online_backup.index).sort_values(by="Online Backup", ascending=False)
+    
+    if save_csv:
+        df_prediction_online_backup.to_csv(path_predictions.joinpath("online_backup.csv"), index="customer_id")
+    
+    return df_prediction_online_backup
+
+
+def predict_tech_support(df, model_path, save_csv=False):
+    df_no_tech_support = df[df["tech_support"] == 0].drop(columns="tech_support")
+
+    # ========== loading model and creating predictions ==========
+    
+    model_tech_support = joblib.load(model_path)
+    
+    pred_prob = model_tech_support.predict_proba(df_no_tech_support)[:, 1]
+
+    # ========== creating prediction dataframe and exporting as a csv ==========
+    
+    df_prediction_tech_support = pd.DataFrame({
+        "Tech Support": pred_prob
+    }, index=df_no_tech_support.index).sort_values(by="Tech Support", ascending=False)
+    
+    if save_csv:
+        df_prediction_tech_support.to_csv(path_predictions.joinpath("tech_support.csv"), index="customer_id")
+    
+    return df_prediction_tech_support
+
+
 predict_fiber(df, path_model_fiber, True)
+
+
 predict_str_tv(df, path_model_str_tv, True)
+
+
 predict_str_movies(df, path_model_str_movies, True)
 
 
+predict_online_backup(df, path_model_online_backup, True)
 
+
+predict_online_security(df, path_model_online_security, True)
+
+predict_tech_support(df, path_model_tech_support, True)
---
+++ src/train_online_security.py
@@ -0,0 +1,126 @@
+from useful_funcs import test_model, filter_column
+import pandas as pd
+import yaml
+import sys
+import lightgbm as lgb
+import joblib
+from pathlib import Path
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder, StandardScaler, FunctionTransformer
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+
+
+# ========== Opening config file ==========
+
+path_yaml = Path("../src/config.yaml")
+try:
+
+    with open(path_yaml, "r") as file:
+        config = yaml.safe_load(file)
+
+except FileNotFoundError:
+    print("Config file not found")
+    sys.exit(1)
+
+
+# ========== opening dataframe and setting paths ==========
+
+path_preprocessed = Path("../data/processed/telco_preprocessed.csv")
+df = pd.read_csv(path_preprocessed, index_col="customer_id")
+
+pd.set_option('future.no_silent_downcasting', True)
+
+
+# ========== Spliting dataframe and formating y axis ==========
+
+X = df.drop(columns = ["online_security"])
+y = df["online_security"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, stratify=y, random_state=42
+)
+
+
+# ========== Creating preprocessor ==========
+
+num_features = config["features"]["numerical_features"]
+ord_features = [config["features"]["ordinal_categoricals"][0]]
+nom_features = config["features"]["nominal_categoricals"]
+ord_feat_categ = [config["features"]["ordinal_orders"][0]]
+col_imp_ranking = config["features"]["columns_ord_shap"]
+
+nom_features.remove("online_security")
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ("num", StandardScaler(), num_features),
+        ("ord", OrdinalEncoder(categories=ord_feat_categ), ord_features),
+         ("nom", OneHotEncoder(drop="first", handle_unknown="ignore", sparse_output=False), nom_features), 
+    ],
+    remainder="drop"
+)
+
+
+preprocessor.set_output(transform="pandas")
+
+
+# ========== Creating optimized model using parameter from optuna ==========
+
+best_params = {
+    "verbose": -1,
+    "learning_rate": 0.034415,
+    "num_leaves": 26,
+    "min_child_samples": 10,
+    "colsample_bytree": 0.563164,
+    "n_estimators": 85,
+    "random_state": 42,
+    "force_row_wise": True
+}
+
+optimized_model = lgb.LGBMClassifier(
+**best_params,
+)
+
+
+# ========== Creating optimized pipeline and default too for comparsion ==========
+
+optimized_pipeline = Pipeline(steps=[
+    ("preprocessor", preprocessor),
+    ("optimized_model", optimized_model)
+]
+)
+optimized_pipeline.fit(X_train, y_train)
+
+default_pipeline = Pipeline(steps=[
+    ("preprocessor", preprocessor),
+    ("default_model", lgb.LGBMClassifier(verbose=-1))
+])
+default_pipeline.fit(X_train, y_train)
+
+
+# ========== Testing and comparing the optimized model ==========
+
+optimized_results = test_model(
+        optimized_pipeline,
+        X_test,
+        y_test,
+        "Optimized Model"
+        )
+
+default_results = test_model(default_pipeline, X_test, y_test, "Default Model")
+
+df_final = pd.concat([default_results, optimized_results], axis=1 )
+
+
+# ========== Dumping the model ==========
+
+path_model = Path(config["paths"]["models"]).joinpath("online_security.pkl")
+
+joblib.dump(optimized_pipeline, path_model)
+
+print(df_final)
+
+
+
+
